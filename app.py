30334050867a05aa080c50edfa6a8e6e31d1909a
kanak.kadulkar <kanakkadulkar@gmail.com>
2025-01-10 17:04:09 +0530

    final

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('vectorize.pkl','rb'))
+model = pickle.load(open('model1.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
